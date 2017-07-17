0664f66e5030be5f623d83b99e72b47e1079e211
iamchiwon <iamchiwon@itsdcode.com>
2017-07-17 15:37:47 +0900

    add state value getter

--- Swiflux/Classes/StoreBase.swift
+++ Swiflux/Classes/StoreBase.swift
@@ -13,6 +13,38 @@ open class StoreBase {
     let _disposeBag = DisposeBag()
     var _states = [String: AnyObject]()
     let _subject = PublishSubject<[String: AnyObject]>()
+    
+    open func state(for key:String) -> AnyObject {
+        if let state = _states[key] {
+            return state
+        }
+        return "" as AnyObject
+    }
+    
+    open func stringState(for key:String) -> String {
+        return state(for:key) as! String
+    }
+    
+    open func boolState(for key:String) -> Bool {
+        if let state = state(for:key) as? Bool {
+            return state
+        }
+        return false
+    }
+    
+    open func intState(for key:String) -> Int {
+        if let state = state(for:key) as? Int {
+            return state
+        }
+        return 0
+    }
+    
+    open func floatState(for key:String) -> Float {
+        if let state = state(for:key) as? Float {
+            return state
+        }
+        return 0
+    }
 
     open func setState(for key: String, state: AnyObject) {
         _states[key] = state
@@ -21,6 +53,18 @@ open class StoreBase {
     }
     
     open func setState(for key: String, state: String) {
+        setState(for:key, state:state as AnyObject)
+    }
+    
+    open func setState(for key: String, state: Bool) {
+        setState(for:key, state:state as AnyObject)
+    }
+    
+    open func setState(for key: String, state: Int) {
+        setState(for:key, state:state as AnyObject)
+    }
+    
+    open func setState(for key: String, state: Float) {
         setState(for:key, state:state as AnyObject)
     }
 
